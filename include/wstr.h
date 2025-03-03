32396d367accb01d433083b0a854cc6afa5f3bb7
without-eyes <dimka180205@gmail.com>
2025-03-03 20:36:38 +0200

    Add documentation comments for calculate_checksum and wstr functions

--- include/wstr.h
+++ include/wstr.h
@@ -9,8 +9,25 @@
 #ifndef WSTR_H
 #define WSTR_H
 
+/**
+    * Computes the Internet checksum for a given buffer.
+    *
+    * @param[in] buffer The pointer to data buffer.
+    * @param[in] length The length of the buffer in bytes.
+    *
+    * @return Returns computed cheksum.
+*/
 unsigned short calculate_checksum(void *buffer, int length);
 
+/**
+    * Performs a traceroute to the specified destination host.
+    *
+    * @param[in] destinationHost The destination host's domain
+    * name or IP address.
+    *
+    * @note This function requires raw socket privileges, so
+    * it need to be executed with root permissions.
+*/
 void wstr(const char *destinationHost);
 
 #endif //WSTR_H
